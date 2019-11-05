9ee8ea7a82586502f224de311cd7f896e546eecb
Kousik Rajesh <kousik18@iitg.ac.in>
2019-11-05 13:38:32 +0530

    Updated Lab12

--- Lab12/mst.cpp
+++ Lab12/mst.cpp
@@ -2,81 +2,99 @@
 using namespace std; 
 typedef long long lli;
 typedef long double lld;
-
+#define mod 1000000007
+#define fori(n) for(lli i=0;i<n;i++)
 const int N=1e6+100;
-vector<int> parent(N,0);
-vector<int> ranks(N,0);
-vector<int> value(N,0);
+vector<lli> parent(N);
+vector<lli> ranks(N);
+vector<lli> value(N);
 struct edge{
-	int x,y;
-	int w;
+	lli x,y;
+	lli w;
 };
 bool compare(edge e1,edge e2){
-	return e1.w<e2.w;
+	return e1.w>e2.w;
 
 }
-int find_set(int val){
-	if(parent[val]==val) return val;
-	else return parent[val]=find_set(val);
+lli find_set(lli val){
+	
+	vector<lli> A;
+	while(val != parent[val])
+	{
+	    A.push_back(val);
+	    val = parent[val];
+	}
+	for(lli i=0; i < A.size(); i++) 
+		parent[A[i]] = val;
+	return val;
 
 }
-int make_set(int val){
+lli make_set(lli val){
 	parent[val]=val;
 	ranks[val]=0;
 }
-bool check_same(int a, int b){
+bool check_same(lli a, lli b){
 	if(find_set(a)==find_set(b)) return true;
 	return false;
 
 }
-int union_sets(int a ,int b){
+void union_sets(lli a ,lli b){
 	a=find_set(a);
 	b=find_set(b);
-	if(a!=b){
-		if(ranks[a]==ranks[b]) ranks[b]++;
-		if(ranks[a]<=ranks[b]){
-			parent[a]=b;
-			return b;
-		}
-		else {
-			parent[b]=a;
-			return a;
-		}
+	if(ranks[a] > ranks[b])
+	{
+	    parent[b] = a;
 	}
+	else if(ranks[a] < ranks[b])
+	{
+	    parent[a] = b;
+	} 
+	else 
+	{    
+	    parent[b] = a;
+	    ranks[a] += 1;
+	}
+	
 		
 
 }
 int main(){
-	int n,m;
-	int a,b,c;
+	lli n,m;
+	lli a,b,c;
 	cin>>n>>m;
-	vector<edge> graph(m+1);
+	vector<edge> graph(m);
 	//cout<<"co";
-	for(int i=0;i<m;i++){
+	for(lli i=0;i<m;i++){
 		cin>>a>>b>>c;
-		edge temp;
-		temp.x=a;
-		temp.y=b;
-		temp.w=c;
-		graph[i]=temp;
+		
+		graph[i].x=a;
+		graph[i].y=b;
+		graph[i].w=c;
+		
+
 		//cout<<graph[i].x<<" "<<graph[i].y<<" "<<graph[i].w<<endl;
 		
 	}
-	cout<<"ao";
+	
 	sort(graph.begin(),graph.end(),compare);
-	cout<<"bo";
-	vector<edge> result;
-	int product=1;
-	int edges_chosen=0;
-	int edge_index=0;
-	cout<<"yo";
-	while(edges_chosen<n-1 && edge_index<m){
-		edge temp=graph[edge_index];
-		if(!check_same(temp.x,temp.y)){
-			result.push_back(temp);
-			product*=temp.w;
-			union_sets(temp.x,temp.y);
+	
+	
+	lli product=1;
+
+	
+	fori(n){
+		make_set(i);
+	}
+	fori(m){
+		
+		
+		if(!check_same(graph[i].x,graph[i].y)){
+			
+			product=((product)%mod*(graph[i].w)%mod)%mod;
+			union_sets(graph[i].x,graph[i].y);
+			
 		}
+		
 	}
 	cout<<product<<endl;
 
